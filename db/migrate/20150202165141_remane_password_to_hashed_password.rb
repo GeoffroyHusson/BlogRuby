6a9d8fded6a86767e7df1c03d3099c59fc8ce204
Geoffroy H <geoffroy_husson@hotmail.com>
2015-02-04 11:41:28 +0100

    bug javascript

--- db/migrate/20150202165141_remane_password_to_hashed_password.rb
+++
@@ -1,8 +0,0 @@
-class RemanePasswordToHashedPassword < ActiveRecord::Migration
-  def self.up
-  	rename_column :users, :password, :hashed_password
-  end
-  def self.down
-  	rename_column :users, :hashed_password, :password
-  end
-end
